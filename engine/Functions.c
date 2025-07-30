42f3332638faf5efe25ffbec09a4c3e1a1e32090
Samuel Adeosun <olusanmi.adeosun@gmail.com>
2025-07-30 12:44:52 +0100

    ...

--- engine/Functions.c
+++ engine/Functions.c
@@ -87,20 +87,30 @@ float *vec3_largedot(vec3_t *aptr, vec3_t *bptr, size_t len){
 	}
 	return out;
 }
-float *vec3_largefdot(vec3_t *aptr, float *bptr, const size_t bptrlen, size_t len){
-	if(bptrlen != len* 3){return NULL;}
-	float *out = _aligned_malloc(len * sizeof(float), 16);
+/// @brief Compute multiple dot product operations with a Vector3 and floats(that are treated as Vector3). 
+/// It functions by computing each multiplication locally and using SIMD to add the results, The SIMD __m128s are cfilled on a per-axis basis, 
+/// where one axis fills the whole of the __m128 and 4 floats can be handled.
+/// @param aptr The Vectors to be dotted with.
+/// @param bptr The floats that will be treated as Vector3s.
+/// @param bptrlen The length of the bptr array 
+/// @param aptrlen The length of the aptr array.
+/// @remark bptrlen MUST BE AT LEAST A MULTIPLE OF 3,
+/// The length of bptr must at least be a multiple of 3, if aptr or bptr are longer then the Program will adapt itself to ensure all Vector3s have a dot function applied.
+/// @return The dot product result of each Vector3 and float(Vector3), will be the same Length as aptrlen.
+float *vec3_largefdot(vec3_t *aptr, float *bptr, const size_t bptrlen, size_t aptrlen){
+	if(bptrlen%3 != 0){return NULL;}
+	float *out = _aligned_malloc(aptrlen * sizeof(float), 16);
 	__m128 _1, _2, _3;
 	float temp[4];
-	uint8_t cc_;
-	for(size_t cc =0; cc < len; cc++){
+	size_t cc_12 =0;
+	for(size_t cc_4 =0; cc_4 < aptrlen && cc_12 < bptrlen; cc_4=(cc_4 == aptrlen?0: cc_4+4), cc_12=(cc_12 == bptrlen?0: cc_12+12)){
 		//X
-		_1 =_mm_load_ps((float[4]){aptr[cc].x* bptr[cc], (cc < len-1? aptr[cc+1].x* bptr[cc+1]: 0), (cc < len-2? aptr[cc+2].x* bptr[cc+2]: 0), (cc < len-3? aptr[cc+3].x* bptr[cc+3]: 0)});
+		_1 =_mm_load_ps((float[4]){aptr[cc_4].x* bptr[cc_12], (cc_4 < aptrlen-1? aptr[cc_4+1].x* bptr[cc_12+1]: 0), (cc_4 < aptrlen-2? aptr[cc_4+2].x* bptr[cc_12+2]: 0), (cc_4 < aptrlen-3? aptr[cc_4+3].x* bptr[cc_12+3]: 0)});
 		//Y
-		_2 =_mm_load_ps((float[4]){aptr[cc].y* bptr[cc], (cc < len-1? aptr[cc+1].y* bptr[cc+1]: 0), (cc < len-2? aptr[cc+2].y* bptr[cc+2]: 0), (cc < len-3? aptr[cc+3].y* bptr[cc+3]: 0)});
+		_2 =_mm_load_ps((float[4]){aptr[cc_4].y* bptr[cc_12+1], (cc_4 < aptrlen-1? aptr[cc_4+1].y* bptr[cc_12+2]: 0), (cc_4 < aptrlen-2? aptr[cc_4+2].y* bptr[cc_12+3]: 0), (cc_4 < aptrlen-3? aptr[cc_4+3].y* bptr[cc_12+4]: 0)});
 		//Z
-		_3 =_mm_load_ps((float[4]){aptr[cc].z* bptr[cc], (cc < len-1? aptr[cc+1].z* bptr[cc+1]: 0), (cc < len-2? aptr[cc+2].z* bptr[cc+2]: 0), (cc < len-3? aptr[cc+3].z* bptr[cc+3]: 0)});
-		_mm_store_ps(out+cc, _mm_add_ps(_1, _mm_add_ps(_2, _3)));
+		_3 =_mm_load_ps((float[4]){aptr[cc_4].z* bptr[cc_12+2], (cc_4 < aptrlen-1? aptr[cc_4+1].z* bptr[cc_12+3]: 0), (cc_4 < aptrlen-2? aptr[cc_4+2].z* bptr[cc_12+4]: 0), (cc_4 < aptrlen-3? aptr[cc_4+3].z* bptr[cc_12+5]: 0)});
+		_mm_store_ps(out+cc_4, _mm_add_ps(_1, _mm_add_ps(_2, _3)));
 	}
 	return out;
 }
@@ -166,6 +176,9 @@ vec3_t *vec3_largesub(vec3_t *vptr, vec3_t *vptr2, const size_t size){
 	return out;
 }
 
+pvec3_t pvvec3_div(pvec3_t a, pvec3_t b){return (pvec3_t){a.x/b.x, a.y/b.y, a.z/b.z};}
+pvec3_t pvec3_div(pvec3_t a, float b){return (pvec3_t){a.x/b, a.y/b, a.z/b};}
+
 float pvec3_dot(pvec3_t a, pvec3_t b){return (a.x*b.x)+ (a.y*b.y)+ (a.z*b.z);}
 float pvec3_fdot(pvec3_t a, float *b){return (a.x*b[0])+ (a.y*b[1])+ (a.z*b[2]);}
 
@@ -176,8 +189,6 @@ pvec3_t pvec_normalise(pvec3_t pv){return pvec3_div(pv, pvec_mag(pv));}
 pvec3_t pvec3_mul(pvec3_t v, float mul){return (pvec3_t){v.x*mul, v.y*mul, v.z*mul};}
 pvec3_t pvvec3_mul(pvec3_t v, pvec3_t mul){return (pvec3_t){v.x*mul.x, v.y*mul.y, v.z*mul.z};}
 
-pvec3_t pvvec3_div(pvec3_t a, pvec3_t b){return (pvec3_t){a.x/b.x, a.y/b.y, a.z/b.z};}
-pvec3_t pvec3_div(pvec3_t a, float b){return (pvec3_t){a.x/b, a.y/b, a.z/b};}
 
 pvec3_t pvec3_avg(pvec3_t a, pvec3_t b){return pvec3_div(pvec3_add(a, b), 2);}
 #pragma endregion
@@ -197,12 +208,25 @@ matrix_t *matrix_init(float *data, size_t arrsize, size_t xsize, size_t ysize){
 	}
 }
 
-vec3_t matrix_vec3mul(matrix_t *m, const size_t mlen, vec3_t *v, const size_t vlen){
-	for(size_t vcc =0; vcc < vlen; ++vcc){
-		for(size_t mcc =0; mcc < mlen; ++mcc){
-			
+vec3_t *matrix_vec3mul(matrix_t *m, const size_t mlen, vec3_t *v, const size_t vlen){
+	vec3_t *out = alloca(sizeof(vec3_t)* vlen);
+	size_t cc =0;
+	//Handles it as for each vertex, handle the matrices.
+	float *columns = alloca(sizeof(float));
+	for(size_t mcc =0; mcc < mlen; ++mcc){
+		if(m[mcc].ysize != 3){break;}
+		columns = realloca(sizeof(float)* m[cc].xsize);
+		for(size_t mx =0; mx < m[mcc].xsize; mx+=3){
+			//For each X component (because The Y has to be 3) all the values at Y are put into Column.
+			columns[mx] = m[mcc]._mesh[mx][0]; columns[mx+1] = m[mcc]._mesh[mx][1]; columns[mx+1] = m[mcc]._mesh[mx][2];
+		}
+		//Compile each Matrix once then transform each Vector3 by it.
+		for(size_t vcc =0; vcc < vlen; ++vcc){
+			column = vec3_largefdot(v, columns, );
+			//Avoid any wrongly sized matrices.
 		}
 	}
+	free(columns);
 }
 
 
