551f70eda335150abc7efa681a222c7794892293
Samuel Adeosun <olusanmi.adeosun@gmail.com>
2025-08-01 13:41:47 +0100

    ...

--- engine/Graphics/Window/Window.c
+++ engine/Graphics/Window/Window.c
@@ -1,4 +1,3 @@
-typedef struct{
 #ifdef _WIN32 // note the underscore: without it, it's not msdn official!
     // Windows (x64 and x86)
 #elif __unix__ // all unices, not all compilers
@@ -8,12 +7,3 @@ typedef struct{
 #elif __APPLE__
     // Mac OS, not sure if this is covered by __posix__ and/or __unix__ though...
 #endif
-
-// #ifdef WIN32 || __MINGW32__ || __MINGW64__
-
-// #elif __LINUX__
-
-// #elif __APPLE__
-
-// #endif
-}
