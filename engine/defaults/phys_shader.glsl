95b096d37a790da8d49f408cff839e2c5d73d72a
Sam <olusanmi.adeosun@gmail.com>
2025-11-26 12:12:42 +0000

    Update phys_shader.glsl
    Remved the originl pos0, ldot0 and pos1, ldot1 uniforms replaced with attributes.

--- engine/defaults/phys_shader.glsl
+++ engine/defaults/phys_shader.glsl
@@ -3,17 +3,17 @@
 #define vs
 // Compare 2 gameobj's against each other as well as every other one.
 
-layout(location = 1) vec3 target_pos;
-layout(location = 2) vec3 target_ldot;
+layout(location = 1) vec3 pos0;
+layout(location = 2) vec3 ldot0;
 
-uniform unsigned int counter;
+layout(location = 1) vec3 pos1;
+layout(location = 2) vec3 ldot1;
+
+layout(location = 1) vec3 pos2;
+layout(location = 2) vec3 ldot2;
+
+layout(location = 2) unsigned int counter;
 uniform unsigned int width;
-
-uniform vec3 pos0;
-uniform float ldot0;
-
-uniform vec3 pos1;
-uniform float ldot1;
 
 // a: pos0 and pos1 collide, r: pos0 and target collide, g: pos1 and target collide, b: IDK
 out bool success1, success2, success3;
@@ -21,7 +21,7 @@ void main(){
     success1 = dot(pos0, pos1) < ldot0 + ldot1;
     success2 = dot(pos0, target_pos) < ldot0 + target_ldot;
     success3 = dot(pos1, target_pos) < ldot1 + target_ldot;
-    gl_Position = vec4(counter, counter % width, 1, 1);
+    gl_Position = vec4(counter/3, (counter/3) % width, 1, 1);
 }
 #define shaderend
 
