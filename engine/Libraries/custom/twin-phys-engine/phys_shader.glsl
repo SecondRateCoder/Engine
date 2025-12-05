9a0f0dac0af0fb7e79dad05d799712d6a2b9734e
Sam <olusanmi.adeosun@gmail.com>
2025-12-05 12:22:09 +0000

    Enhance phys_shader.glsl with additional position checks
    Accomodated for the 2 extra attributes.

--- engine/Libraries/custom/twin-phys-engine/phys_shader.glsl
+++ engine/Libraries/custom/twin-phys-engine/phys_shader.glsl
@@ -12,23 +12,31 @@ layout(location = 1) vec3 ldot1;
 layout(location = 2) vec3 pos2;
 layout(location = 3) vec3 ldot2;
 
-layout(location = 4) uvec2 counter;
+layout(location = 4) vec3 pos3;
+layout(location = 5) vec3 ldot3;
+
+layout(location = 6) vec3 pos4;
+layout(location = 7) vec3 ldot4;
+
+layout(location = 8) unsigned int counter;
 uniform unsigned int width; // Store size_t in encoded state.
 
-// a: pos0 and pos1 collide, r: pos0 and target collide, g: pos1 and target collide, b: IDK
-out bool success1, success2;
+// a: pos0 and pos1 collide, r: pos0 and pos2 collide, g: pos0 and pos3 collide, b: pos4
+out bool success1, success2, success3, success4;
 void main(){
     success1 = dot(pos0, pos1) < ldot0 + ldot1;
     success2 = dot(pos0, pos2) < ldot0 + ldot2;
-    gl_Position = vec4(counter.x/2, (counter.x/2) % width, 1, 1);
+    success3 = dot(pos0, pos3) < ldot0 + ldot3;
+    success4 = dot(pos0, pos4) < ldot0 + ldot4;
+    gl_Position = vec4((counter > width? counter%width: counter) counter/width, 1, 1);
 }
 #define shaderend
 
 #define fs
 
-in bool success1, success2;
+in bool success1, success2, success3, success4;
 
 void main(){
-    FragColor = vec4(success1, success2, 0, 0);
+    FragColor = vec4(success1, success2, success3, success4);
 }
 #define shaderend
